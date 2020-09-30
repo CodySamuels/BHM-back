f9a0635a5dc38fe3a44568b224ca7e15a847ea85
Cody Samuels <twowoodenspoons@gmail.com>
2020-09-30 12:28:12 -0700

    updated user model with new parameters

--- models/user.js
+++ models/user.js
@@ -1,39 +1,67 @@
 // DEPENDENCIES AND VARIABLES
 // ===============================================
+const Sequelize = require("sequelize");
 const bcrypt = require("bcrypt");
 
 
 // MODEL
 // ===============================================
-module.exports = function (sequelize, DataTypes) {
+module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define("user", {
+
+    userId: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
 
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
       unique: true,
-      allowNull: false
+      isEmail: true
     },
 
-    password: DataTypes.STRING,
-    name: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      is: /^[0-9a-f]{64$/i,
+      validate: {
+        len: [6]
+      }
+    },
+
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
     address: DataTypes.STRING,
     city: DataTypes.STRING,
     state: DataTypes.STRING,
     zip: DataTypes.STRING,
     phoneNumber: DataTypes.STRING,
-  }, {
-    tableName: "user"
-  })
+  },
 
-  user.beforeCreate(function (user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-  })
 
-  user.associate = function (models) {
-    user.hasOne(models.cart, {
-      onDelete: "cascade"
-    });
+    // TABLE NAME
+    // ===============================================
+    {
+      tableName: "user"
+    })
+
+
+  // ASSOCIATIONS
+  // ===============================================
+  user.associate = (models) => {
+    user.hasOne(models.cart, { onDelete: "cascade" });
   };
 
+
+  // HASH AND SALT THE USER'S PASSWORD
+  // ===============================================
+  user.beforeCreate((user) => { user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null) })
+
+
+  // RETURN
+  // ===============================================
   return user;
 }
